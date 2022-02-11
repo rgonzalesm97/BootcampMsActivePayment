62f5220ce1cb10825108485170af2b398a5023a4
Rodrigo Gonzales <rgonzalesm1997@gmail.com>
2022-02-11 16:15:16 -0500

    first commit

---
+++ src/main/java/com/bank/activePayment/ActivePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.bank.activePayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivePaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/activePayment/ActivePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.activePayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
